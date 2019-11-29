3119bfdb4643e0f34a798d29a73657d64a5ef8ed
Inez <iminezj@gmail.com>
2019-11-29 21:51:05 +0100

    Updated settings

--- fodmap_website/settings.py
+++ fodmap_website/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
+# STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -166,8 +166,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-
 
 django_heroku.settings(locals())
