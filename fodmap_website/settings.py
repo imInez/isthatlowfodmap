9d8691af8396c294a3110006d32cbda983b5a38c
Inez <iminezj@gmail.com>
2019-11-29 21:25:58 +0100

    Updated settings

--- fodmap_website/settings.py
+++ fodmap_website/settings.py
@@ -166,9 +166,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 
 
 django_heroku.settings(locals())
